ba581abd9a5b916d14b00eacbfcb8372b1ba949b
Sebastian Meier <sebastiandmeier@gmail.com>
2026-08-12 16:14:26 +0200

    Add program abstractions

--- src/SdlSandbox/Program.cs
+++ src/SdlSandbox/Program.cs
@@ -4,48 +4,85 @@ namespace SdlSandbox;
 
 internal static class Program {
 
-    private static IntPtr _window;
-    private static BitmapTexture? _texture;
-    private static RenderContext? _context;
+    private static bool mRunning = true;
+    private static IntPtr mWindow;
+    private static BitmapTexture? mTexture;
+    private static RenderContext? mContext;
+
 
     [STAThread]
     private static int Main(string[] args) {
-        if (!SDL.Init(SDL.InitFlags.Video))
-            return -1;
-
-
-        _context = RenderContext.Create(
-            "appName",
-            "appIdentifier",
-            "windowTitle",
-            1200,
-            900);
-        _texture = BitmapTexture.Create(_context.Renderer, 128, 128, "Images/Default.png");
-
-
-        var running = true;
-
-        while (running) {
-            while (SDL.PollEvent(out SDL.Event e)) {
-                if (e.Type == ((uint)SDL.EventType.Quit))
-                    running = false;
-            }
-            RenderFrame(_context);
-
-            SDL.Delay(16);
-        }
-        _context.Dispose();
+        Initialize();
+        MainLoop();
         Cleanup();
         SDL.Quit();
 
         return 0;
     }
 
+    private static void Initialize() {
+        if (!SDL.Init(SDL.InitFlags.Video))
+            throw new InvalidOperationException("Failed to initialize SDL.");
+
+        mRunning = true;
+        mContext = RenderContext.Create(
+            "appName",
+            "appIdentifier",
+            "windowTitle",
+            1200,
+            900);
+        mTexture = BitmapTexture.Create(mContext.Renderer, 128, 128, "Images/Default.png");
+    }
 
 
-    private static void RenderFrame(RenderContext context) {
+    private static void MainLoop() {
 
-        var texture = _texture ?? throw new InvalidOperationException("Texture was not created.");
+        int fps = 0;
+        ulong lastTime = 0;
+
+        while (mRunning) {
+
+            var currentTick = SDL.GetTicks();
+            Tick();
+            fps++;
+            var deltatime = SDL.GetTicks() - currentTick;
+            if (currentTick - lastTime >= 1000) {
+                SDL.SetWindowTitle(mContext.Window, $"FPS: {fps}");
+                fps = 0;
+                lastTime = currentTick;
+            }
+            //SDL.Delay(16);
+        }
+    }
+
+    private static void Tick() {
+        // Do a frame
+        Input();
+        Update();
+        Render(mContext);
+    }
+
+    private static void Input() {
+        // Handle input events
+        while (SDL.PollEvent(out SDL.Event e)) {
+            switch (e.Type) {
+                case (uint)SDL.EventType.Quit:
+                    mRunning = false;
+                    break;
+                case (uint)SDL.EventType.KeyDown:
+                    // Handle keyboard events here
+                    break;
+            }
+        }
+    }
+
+    private static void Update() {
+        // Update game logic
+    }
+
+    private static void Render(RenderContext context) {
+        // Render the scene
+        var texture = mTexture ?? throw new InvalidOperationException("Texture was not created.");
         var ticks = SDL.GetTicks();
         var direction = (ticks % 2000) >= 1000 ? 1.0f : -1.0f;
         var scale = ((((int)(ticks % 1000)) - 500) / 500.0f) * direction;
@@ -72,9 +109,12 @@ internal static class Program {
         SDL.RenderPresent(context.Renderer);
     }
 
+
     private static void Cleanup() {
-        _texture?.Dispose();
-        _texture = null;
+        mTexture?.Dispose();
+        mTexture = null;
+        mContext?.Dispose();
+        mContext = null;
     }
 
 }
