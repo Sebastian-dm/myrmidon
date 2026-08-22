c78725c1155ef55fe445794561e03026fe1ae5f4
Sebastian-dm <sebm@tuta.com>
2026-08-22 21:44:46 +0200

    merge

--- src/Myrmidon.App/Render/Terminal.cs
+++ src/Myrmidon.App/Render/Terminal.cs
@@ -3,6 +3,7 @@
 using SDL3;
 
 using Bramble.Core;
+using Myrmidon.Terminal;
 using Myrmidon.Core.Game;
 using Myrmidon.Core.Entities;
 using Myrmidon.Core.Maps.Tiles;
@@ -14,14 +15,10 @@ public class Renderer : IDisposable {
     private static IntPtr _window;
     private static IntPtr _renderer;
     private FpsCounter _fpsCounter;
-
-    private List<ISubRenderer> _subRenderers = new List<ISubRenderer>();
-        
-    private GameState _gameState;
     
-    public Renderer(FpsCounter fpsCounter,GameState gameState) {
+    
+    public Renderer(FpsCounter fpsCounter) {
         _fpsCounter = fpsCounter;
-        _gameState = gameState;
         if (!SDL.Init(SDL.InitFlags.Video))
             throw new InvalidOperationException("Failed to initialize SDL.");
 
@@ -44,32 +41,20 @@ public class Renderer : IDisposable {
         
         SDL.SetRenderLogicalPresentation(_renderer, 320, 240, SDL.RendererLogicalPresentation.Letterbox);
         SDL.SetRenderVSync(_renderer, 1);
-
-        RegisterSubRenderer();
-    }
-
-    private void RegisterSubRenderer() {
-        _subRenderers.Add(new SceneRenderer(_renderer, new SDL.Rect { X = 0, Y = 0, W = 280, H = 200 }, _gameState));
     }
 
 
-    public void Render() {
+    public void Render(GameState context) {
         // Clear
-        SDL.SetRenderViewport(_renderer, IntPtr.Zero);
         SetRenderDrawColor("DarkGray");
         SDL.RenderClear(_renderer);
-
         
-        foreach (var subRenderer in _subRenderers) {
-            subRenderer.Render();
-        }
-
         DrawFpsText(1f, 1f, 2);
         
         SDL.RenderPresent(_renderer);
     }
 
-    public static void SetRenderDrawColor(string color, byte? alpha = null) {
+    private static void SetRenderDrawColor(string color, byte? alpha = null) {
         Color c = TerminalColor.ToSystemColor(color);
         SDL.SetRenderDrawColor(_renderer, c.R, c.G, c.B, alpha ?? c.A);
     }
@@ -85,7 +70,7 @@ public class Renderer : IDisposable {
     }
     
     
-    private void DrawTestScreen(GameState context) {
+    private void DrawTestScene(GameState context) {
         // _testSurface = SDL.LoadPNG("Images/Default.png");
         // // Render the scene
         // var texture = mTexture ?? throw new InvalidOperationException("Texture was not created.");
@@ -114,6 +99,55 @@ public class Renderer : IDisposable {
         //
         // SDL.RenderPresent(_renderer);
     }
+    
+    
+    private void RenderScene(GameState context) {
+        // TerminalColor backgroundColor = TerminalColor.Black;
+        // //terminal.Clear();
+        //
+        // var map = context.Hectare.Map;
+        // if (map == null) return;
+        //
+        // Vec center = new Vec(context.Hectare.Player.Position.X, context.Hectare.Player.Position.Y);
+        // Rect viewBounds = new Rect(center - terminal.Size/2, terminal.Size);
+        //
+        // // Paint tiles
+        // for (int y = viewBounds.Top; y < viewBounds.Bottom; y++) {
+        //     for (int x = viewBounds.Left; x < viewBounds.Right; x++) {
+        //         if (!IsInMapBounds(x, y, map)) continue;
+        //
+        //         var tile = map.GetTileAt<Tile>(x, y);
+        //
+        //         var screenPos = new Vec(x - viewBounds.Left, y - viewBounds.Top);
+        //         terminal[screenPos.X, screenPos.Y][TerminalColor.Gray, backgroundColor].Write(tile.Glyph);
+        //
+        //     }
+        // }
+        //
+        // //Paint entities
+        // foreach (var entity in map.Entities.Items) {
+        //
+        //     if (entity is Actor actor) {
+        //         if (!IsInMapBounds(actor.Position.X, actor.Position.Y, map)) continue;
+        //         if (!IsInViewBounds(actor.Position.X, actor.Position.Y, viewBounds)) continue;
+        //         int screenX = actor.Position.X - viewBounds.Left;
+        //         int screenY = actor.Position.Y - viewBounds.Top;
+        //         terminal[screenX, screenY][TerminalColor.ToSystemColor("LightRed"), backgroundColor].Write(actor.Glyph);
+        //     }
+        // }
+        //
+        // // Paint player
+        // if (context.Hectare.Player != null) {
+        //     var playerPos = new Vec(context.Hectare.Player.Position.X - viewBounds.Left, context.Hectare.Player.Position.Y - viewBounds.Top);
+        //     terminal[playerPos.X, playerPos.Y][TerminalColor.LightGreen, backgroundColor].Write(context.Hectare.Player.Glyph);
+        // }
+    }
+    
+
+    private bool IsInViewBounds(int x, int y, Rect viewBounds) {
+        return x >= viewBounds.Left && x < viewBounds.Right && y >= viewBounds.Top && y < viewBounds.Bottom;
+    }
+
 
     public void Dispose() {
         SDL.DestroyWindow(_window);
