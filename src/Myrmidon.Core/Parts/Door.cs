88a08eb454299585e7a1004be36c4d79ded6537a
Sebastian Meier <sebastiandmeier@gmail.com>
2026-09-18 09:55:32 +0200

    Add part and system for handling doors and entities

---
+++ src/Myrmidon.Core/Parts/Door.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Myrmidon.Core.Parts {
+    internal class Door : Part {
+
+
+        public bool IsLocked; // Locked door = 1, Unlocked = 0
+        public bool IsClosed; // Open door = 1, closed = 0
+
+
+        //Default constructor
+        //A Door can be set locked/unlocked/open/closed using the constructor.
+        public Door(bool isLocked, bool isClosed) {
+
+            IsLocked = isLocked;
+            IsClosed = isClosed;
+        }
+    }
+}
---
+++ src/Myrmidon.Core/Systems/DoorSystem.cs
@@ -0,0 +1,66 @@
+﻿using Myrmidon.Core.ECS;
+using Myrmidon.Core.Parts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myrmidon.Core.Systems {
+    internal class DoorSystem {
+
+        private Ecs _ecs;
+
+
+        public DoorSystem(Ecs ecs) {
+            _ecs = ecs;
+        }
+
+
+        public void Open(EntityId entity) {
+            if (!_ecs.Has<Door>(entity) || !_ecs.Has<Physics>(entity))
+                return;
+            var door = _ecs.Get<Door>(entity);
+            
+            if(!door.IsLocked) {
+                door.IsClosed = false;
+
+                var physics = _ecs.Get<Physics>(entity);
+                physics.BlocksMovement = false;
+                physics.BlocksLineOfSight = false;
+            }
+        }
+
+
+        public void Close(EntityId entity) {
+            if (!_ecs.Has<Door>(entity) || !_ecs.Has<Physics>(entity))
+                return;
+            
+            var door = _ecs.Get<Door>(entity);
+            door.IsClosed = true;
+
+            var physics = _ecs.Get<Physics>(entity);
+            physics.BlocksMovement = true;
+            physics.BlocksLineOfSight = true;
+        }
+
+
+        public void Lock(EntityId entity) {
+            if (!_ecs.Has<Door>(entity) || !_ecs.Has<Physics>(entity))
+                return;
+
+            var door = _ecs.Get<Door>(entity);
+            if (door.IsClosed)
+                door.IsLocked = true;
+        }
+
+
+        public void Unlock(EntityId entity) {
+            if (!_ecs.Has<Door>(entity) || !_ecs.Has<Physics>(entity))
+                return;
+            var door = _ecs.Get<Door>(entity);
+            door.IsLocked = false;
+        }
+
+    }
+}
