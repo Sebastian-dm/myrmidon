9390c6cf0096ec2b7126aab900ed01ae52aba87d
Sebastian Meier <sebastiandmeier@gmail.com>
2026-09-18 09:55:17 +0200

    Add physics part

---
+++ src/Myrmidon.Core/Parts/Physics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myrmidon.Core.Parts {
+    internal class Physics : Part {
+        public bool BlocksMovement;
+        public bool BlocksLineOfSight;
+    }
+}
