67bf9fa4440577a5449ad6e96ccb23eb2c95609b
Sebastian Meier <sebastiandmeier@gmail.com>
2026-09-22 10:59:41 +0200

    Add groups for identification

--- src/Myrmidon.Core/Parts/Identity.cs
+++ src/Myrmidon.Core/Parts/Identity.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Myrmidon.Core.Parts {
-    internal class Identity {
+namespace Myrmidon.Core.Parts;
 
-        public string Name { get; set; } = "Unknown";
-    }
+internal class Identity {
+
+    public string Name { get; set; } = "Unknown";
+
+    public List<string> Groups { get; set; } = new List<string>();
 }
