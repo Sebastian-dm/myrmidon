c8b8af6764ca3bf4b74ef29d65350461245ecaac
Sebastian Meier <sebastiandmeier@gmail.com>
2026-09-22 10:11:26 +0200

    Add tile factory

---
+++ src/Myrmidon.Core/Ecs/EntityTileFactory.cs
@@ -0,0 +1,47 @@
+﻿using Bramble.Core;
+using Myrmidon.Core.Parts;
+using Myrmidon.Core.Zones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Myrmidon.Core.ECS;
+
+
+public  class EntityTileFactory {
+
+    public EntityTileFactory(Ecs ecs) {
+        _ecs = ecs;
+    }
+
+    private readonly Ecs _ecs;
+
+    public EntityId CreateWall(TileMap map) {
+        EntityId entity = map.Ecs.CreateEntity();
+        _ecs.Add(entity, new Identity { Name = "Wall" });
+        _ecs.Add(entity, new Renderable("tile/wall", (byte)0, "O"));
+        _ecs.Add(entity, new Perceptible());
+        return entity;
+    }
+
+    public EntityId CreateFloor(TileMap map) {
+        EntityId entity = map.Ecs.CreateEntity();
+        _ecs.Add(entity, new Identity { Name = "Floor" });
+        _ecs.Add(entity, new Renderable("text/default", (byte)253, "m"));
+        _ecs.Add(entity, new Perceptible());
+        return entity;
+    }
+
+    public EntityId CreateDoor(TileMap map, bool locked = false, bool closed = true) {
+        EntityId entity = map.Ecs.CreateEntity();
+        _ecs.Add(entity, new Identity { Name = "Door" });
+        _ecs.Add(entity, new Door { IsLocked = locked, IsClosed = closed });
+        _ecs.Add(entity, new Renderable("text/default", (byte)'+', "Y"));
+        _ecs.Add(entity, new Perceptible());
+        return entity;
+    }
+
+}
