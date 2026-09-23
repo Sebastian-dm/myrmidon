1ebaed496d6c1533a7aa3e6f545240e1d72228f0
Sebastian Meier <sebastiandmeier@gmail.com>
2026-09-23 20:18:16 +0200

    Fix door opening sprite variant

--- src/Myrmidon.Core/Behavior/DoorBehavior.cs
+++ src/Myrmidon.Core/Behavior/DoorBehavior.cs
@@ -17,31 +17,36 @@ namespace Myrmidon.Core.Behavior {
         }
 
 
-        public void Open(EntityId entity) {
-            if (!_ecs.Has<Door>(entity) || !_ecs.Has<Physics>(entity))
+        public void Open(EntityId doorEntity) {
+            if (!_ecs.Has<Door>(doorEntity) || !_ecs.Has<Physics>(doorEntity))
                 return;
-            var door = _ecs.Get<Door>(entity);
+            var door = _ecs.Get<Door>(doorEntity);
             
             if(!door.IsLocked) {
                 door.IsClosed = false;
 
-                var physics = _ecs.Get<Physics>(entity);
+                var physics = _ecs.Get<Physics>(doorEntity);
                 physics.BlocksMovement = false;
                 physics.BlocksLineOfSight = false;
+
+                SetVariantByte(doorEntity, [door.IsClosed]);
+
             }
         }
 
 
-        public void Close(EntityId entity) {
-            if (!_ecs.Has<Door>(entity) || !_ecs.Has<Physics>(entity))
+        public void Close(EntityId doorEntity) {
+            if (!_ecs.Has<Door>(doorEntity) || !_ecs.Has<Physics>(doorEntity))
                 return;
             
-            var door = _ecs.Get<Door>(entity);
+            var door = _ecs.Get<Door>(doorEntity);
             door.IsClosed = true;
 
-            var physics = _ecs.Get<Physics>(entity);
+            var physics = _ecs.Get<Physics>(doorEntity);
             physics.BlocksMovement = true;
             physics.BlocksLineOfSight = true;
+
+            SetVariantByte(doorEntity, [door.IsClosed]);
         }
 
 
@@ -60,6 +65,14 @@ namespace Myrmidon.Core.Behavior {
                 return;
             var door = _ecs.Get<Door>(entity);
             door.IsLocked = false;
+        }
+
+        private void SetVariantByte(EntityId entity, bool[] flags){
+            byte n = 0;
+            foreach( var flag in flags)
+                n |= (byte)(flag ? 1 : 0);
+            
+            _ecs.Get<Renderable>(entity).SetVariant(n);
         }
 
     }
