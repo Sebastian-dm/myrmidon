3d4e1e036797bcb873191da28d9716f32ab34c9c
Sebastian Meier <sebastiandmeier@gmail.com>
2026-09-24 16:51:44 +0200

    Add textures

--- src/Myrmidon.Core/Ecs/EntityFactory.cs
+++ src/Myrmidon.Core/Ecs/EntityFactory.cs
@@ -40,7 +40,7 @@ public class EntityFactory {
             NoBlocks = rng.Next(0, 10)
         });
         _world.Add(entity, new Inventory());
-        _world.Add(entity, new Renderable("text/default", (byte)'@', "B", "M"));
+        _world.Add(entity, new Renderable("etc/robert", (byte)22, "white", "B"));
         _world.Add(entity, new Perceptible() {Explored = true, LightLevel = 1.0f, Visible = true});
         return entity;
     }
@@ -62,19 +62,35 @@ public class EntityFactory {
             BlockChance = rng.Next(0, 10),
             NoBlocks = rng.Next(0, 1)
         });
-        _world.Add(entity, new Inventory());
-        _world.Add(entity, new Renderable("etc/robert", (byte)17, "r", colorAccent: "W"));
+
+        var inv = new Inventory();
+        _world.Add(entity, inv);
+
+        EntityId remains = _world.CreateEntity();
+        _world.Add(remains, new Identity { Name = "remains", Groups = new List<string> { "Item" } });
+        _world.Add(remains, new Position { Container = entity });
+        _world.Add(remains, new Renderable("etc/robert", (byte)24, "r", colorAccent: "W"));
+        inv.Items.Add(remains);
+
+
+
+        var tal = rng.Next(0, 100);
+        if (tal > 25)
+            _world.Add(entity, new Renderable("etc/robert", (byte)17, "white", colorAccent: "R"));
+        else
+            _world.Add(entity, new Renderable("etc/robert", (byte)23, "w", colorAccent: "white"));
         _world.Add(entity, new Perceptible());
 
 
         return entity;
     }
 
+
     public EntityId CreateTreasure(int zoneId, Vec position) {
         EntityId entity = _world.CreateEntity();
         _world.Add(entity, new Identity { Name = "a pile of gold", Groups = new List<string> { "Treasure", "Item"} });
         _world.Add(entity, new Position { Coords = position, ZoneId = zoneId });
-        _world.Add(entity, new Renderable("text/default", (byte)'$', "Y"));
+        _world.Add(entity, new Renderable("etc/robert", (byte)21, "y", colorAccent: "W"));
         _world.Add(entity, new Perceptible());
         return entity;
     }
